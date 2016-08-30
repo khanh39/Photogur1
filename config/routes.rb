303d964973e1fc3c3399dfd3e4ae1ee429ec265a
Hamza Khan <hamzakhan@Fatimas-MacBook-Pro-2.local>
2016-08-30 19:36:58 -0400

    Adding feture to edit pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'pictures#index'
-  get 'images' => 'pictures#index'
+  get 'pictures' => 'pictures#index'
 
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
   get 'pictures/:id' => 'pictures#show', as: 'picture'
+  patch 'pictures/:id' => "pictures#update"
 end
 
 
